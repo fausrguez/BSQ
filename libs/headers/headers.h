8d47673a5c2707516ef3a72fa122d5272b184a74
Danai Giannopoulou <dgiannop@student.codam.nl>
2020-08-31 16:16:45 +0200

    added <stdlib.h> to headers Header

--- libs/headers/headers.h
+++ libs/headers/headers.h
@@ -6,13 +6,13 @@
 /*   By: farodrig <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/31 13:18:12 by farodrig      #+#    #+#                 */
-/*   Updated: 2020/08/31 13:39:13 by farodrig      ########   odam.nl         */
+/*   Updated: 2020/08/31 16:16:29 by dgiannop      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HEADERS_H
 # define HEADERS_H
 # include <unistd.h>
-# include <fcntl.h>
+# include <stdlib.h>
 # include <fcntl.h>
 #endif
