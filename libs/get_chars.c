9628e20fd93f86046601e537b7d4168f31d4d4b9
Danai Giannopoulou <dgiannop@student.codam.nl>
2020-09-01 20:44:55 +0200

    fixed get_chars.c for the case of a row number > 9

--- libs/get_chars.c
+++ libs/get_chars.c
@@ -6,7 +6,7 @@
 /*   By: dgiannop <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/31 15:03:06 by dgiannop      #+#    #+#                 */
-/*   Updated: 2020/08/31 16:25:01 by dgiannop      ########   odam.nl         */
+/*   Updated: 2020/09/01 20:44:34 by dgiannop      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,33 @@
 
 char	get_empty_char(char **map_array)
 {
+	int i;
 	char empty;
 
-	empty = map_array[0][1];
+	while (i != '\n')
+		i++;
+	empty = map_array[i - 2];
 	return (empty);
 }
 
-char	get_obst_char(char **map_array)
+char	get_obst_char(char *map_array)
 {
+	int i;
 	char obstacle;
 
-	obstacle = map_array[0][2];
+	while (i != '\n')
+		i++;
+	obstacle = map_array[i - 1];
 	return (obstacle);
 }
 
-char	get_sqr_char(char **map_array)
+char	get_sqr_char(char *map_array)
 {
+	int i;
 	char square;
 
-	square = map_array[0][3];
+	while (i != '\n')
+		i++;
+	square = map_array[i];
 	return (square);
 }
