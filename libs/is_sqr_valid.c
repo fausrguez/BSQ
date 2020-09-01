51c335ad8161016a1b60f6d0e23b3a24e226a9d4
Danai Giannopoulou <dgiannop@student.codam.nl>
2020-09-01 18:01:14 +0200

    fixed is_sqr_valid

--- libs/is_sqr_valid.c
+++ libs/is_sqr_valid.c
@@ -6,14 +6,16 @@
 /*   By: farodrig <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/31 12:00:56 by farodrig      #+#    #+#                 */
-/*   Updated: 2020/09/01 10:14:35 by farodrig      ########   odam.nl         */
+/*   Updated: 2020/09/01 18:00:45 by dgiannop      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "headers/functions.h"
+#include "functions.h"
+#include "headers.h"
 
 int	is_sqr_valid(
 	char **panel,
+	int *panel_size,
 	int *coordinates,
 	int size)
 {
@@ -26,21 +28,17 @@ int	is_sqr_valid(
 	obstacle = get_obst_char(panel);
 	row = coordinates[0];
 	col = coordinates[1];
-	print_char('x');
 	i = 0;
-	j = 0;
-	while (i < size && panel[row + i][col + j] != '\0')
+	while ((i < size) && (i + size <= panel_size[0]))
 	{
-		while (j < size)
+		j = 0;
+		while ((j < size) && (j + size <= panel_size[1]))
 		{
 			if (panel[row + i][col + j] == obstacle)
-			{
 				return (0);
-			}
 			j++;
 		}
 		i++;
 	}
-	print_char('y');
 	return (1);
 }
