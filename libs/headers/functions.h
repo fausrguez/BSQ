2663185f501ec0c0a3197b18856d3595bf11c43b
Danai Giannopoulou <dgiannop@student.codam.nl>
2020-09-02 11:52:30 +0200

    updated functions header file with the updated protypes of functions

--- libs/headers/functions.h
+++ libs/headers/functions.h
@@ -6,7 +6,7 @@
 /*   By: farodrig <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/31 13:27:35 by farodrig      #+#    #+#                 */
-/*   Updated: 2020/09/02 11:21:09 by dgiannop      ########   odam.nl         */
+/*   Updated: 2020/09/02 11:42:16 by dgiannop      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,9 @@ void	add_sqr_to_panel(char **panel, int *coordinates, int size);
 void	print_panel(char **panel, int *panel_size);
 int		get_row_num(char *map_array);
 int		get_col_num(char *map_array);
-char	get_empty_char(char **map_array);
-char	get_obst_char(char **map_array);
-char	get_sqr_char(char **map_array);
+char	get_empty_char(char *map_array);
+char	get_obst_char(char *map_array);
+char	get_sqr_char(char *map_array);
 int		*get_panel_size(char *map_array);
 char	*create_array(char *file_name);
 #endif
