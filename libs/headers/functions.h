87e29c8e347e0ebdb818243c5fc2d281b743df3e
Danai Giannopoulou <dgiannop@student.codam.nl>
2020-09-02 11:23:55 +0200

    "fixed get_panel_size and create_panel"

--- libs/headers/functions.h
+++ libs/headers/functions.h
@@ -6,17 +6,18 @@
 /*   By: farodrig <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/31 13:27:35 by farodrig      #+#    #+#                 */
-/*   Updated: 2020/09/01 12:26:03 by farodrig      ########   odam.nl         */
+/*   Updated: 2020/09/02 11:21:09 by dgiannop      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FUNCTIONS_H
 # define FUNCTIONS_H
 
+int		ft_atoi(char *str);
 void	print_char(char c);
 void	print_str(char *str);
 int		draw_panel(char *panel_file_name);
-char	**create_panel(char *panel_file_name);
+char	**create_panel(char *array);
 int		find_sqr_coordinates(
 	char **panel,
 	int *panel_size,
@@ -30,6 +31,6 @@ int		get_col_num(char *map_array);
 char	get_empty_char(char **map_array);
 char	get_obst_char(char **map_array);
 char	get_sqr_char(char **map_array);
-int		*get_panel_size(char **panel);
+int		*get_panel_size(char *map_array);
 char	*create_array(char *file_name);
 #endif
